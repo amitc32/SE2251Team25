c74aa0fd7a964c2378014583eb482af04a800747
amitc32 <amitc32@uwo.ca>
2023-03-29 16:03:21 -0400

    billboard completed code

---
+++ billboardapi.py
@@ -0,0 +1,19 @@
+import billboard
+
+
+def billboardAPI():
+    chart = billboard.ChartData('hot-100')
+
+    list = []
+    x = chart.__len__()
+    print(x)
+    current = 0
+    new_data = []
+    for current in range(100):
+        raw_data = [(chart[current].artist + ' AND ' + chart[current].title)]
+        for data in raw_data:
+            new_data.append(str(data))
+        current = current + 1
+    return(new_data)
+
+billboardAPI()
